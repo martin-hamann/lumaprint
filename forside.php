feef59ed7c5ba1a7bbc800c60e05a151a00994c5
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 14:08:44 +0100

    frontpage-update

---
+++ forside.php
@@ -0,0 +1,19 @@
+<?php get_header() ?>
+
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post() ?>
+            <h1><?php the_title() ?></h1>
+            <p><?php the_content() ?></p>
+        <?php endwhile ?>
+    <?php endif; ?>
+
+<div class="test-template-part">
+    <?php get_template_part('template-parts/how-it-works/how-it-works'); ?>
+</div>
+<?php get_footer(); ?>
+
+
+<?php get_header(); ?>
+
+
+<?php get_footer(); ?>
