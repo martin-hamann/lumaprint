a7934e7135f40b01dd29f1cd9fa7b839c11ea23e
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 13:52:39 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,11 +4,11 @@
 }
 add_action("init", "ad_remove_gutenberg");
 
-function luma_enqueue_styles() {
+function luma_get_stylesheet() {
     wp_enqueue_style('style', get_stylesheet_uri('style.css'));
     wp_enqueue_style('bootstrap-cdn-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
     wp_enqueue_script('bootstrap-cdn-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js');
 }
 
-add_action('wp_enqueue_scripts', 'luma_enqueue_styles');
+add_action('wp_enqueue_scripts', 'luma_get_stylesheet');
 ?>
