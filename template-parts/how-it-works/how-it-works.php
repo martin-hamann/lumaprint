91e7391bd1180d4a9993393bde75d985cae9ee89
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 13:32:10 +0100

    another template part test

--- template-parts/how-it-works/how-it-works.php
+++ template-parts/how-it-works/how-it-works.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>How-It-Works</title>
     <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
