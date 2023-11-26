92f8213f3f7d141e618b2390f2c72e55933d5b86
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 15:30:39 +0100

    Update header.php

--- header.php
+++ header.php
@@ -13,13 +13,13 @@
 </head>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light headline d-flex justify-content-space-between">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light headline">
         <a class="navbar-brand" href="#"><span class="headline-color">LUMA</span>PRINT</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarText">
+        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Forside</a>
