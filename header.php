b4d3d6ef0e8a25da790b7a112026870bd5867cbe
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 15:40:21 +0100

    navupdate

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarText">
+        <div class="collapse navbar-collapse d-flex justify-content-center id="navbarText">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Forside</a>
