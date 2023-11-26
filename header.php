3febd03cf0245d615d4bd17e4b0aa0b2f76f66f5
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-26 14:28:15 +0100

    navbar added

--- header.php
+++ header.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
 <html <?php language_attributes() ?>>
+
 <head>
     <meta charset="<?php bloginfo("encoding") ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php bloginfo("LUMAPRINT") ?></title>
+    <title>
+        <?php bloginfo("LUMAPRINT") ?>
+    </title>
     <?php wp_head() ?>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><span>LUMA</span>PRINT</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+            aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+            </ul>
+            <span class="navbar-text">
+                Navbar text with an inline element
+            </span>
+        </div>
+    </nav>
+</header>
+
 <body>
