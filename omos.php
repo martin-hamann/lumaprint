138c377c68c4600e93bba479b834e2a5ef994951
Kajmak101 <Adnan_s@live.dk>
2023-11-26 14:56:04 +0100

    test form

--- omos.php
+++ omos.php
@@ -1,1 +1,10 @@
-<?php echo do_shortcode('[contact-form-7 id="cc77310" title="Contact form 1"]') ?>
+<?php get_header();
+?>
+
+<div class="registration-form">
+    <?php echo do_shortcode('[contact-form-7 id="cc77310" title="Contact form 1"]') ?>
+</div>
+
+<?php get_footer(); ?>
+
+
