3978882f7e8f9c43b1c4c62802bcbbb22388ef84
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-27 11:04:38 +0100

    func-navbar next attempt

--- functions.php
+++ functions.php
@@ -5,9 +5,9 @@
 add_action("init", "ad_remove_gutenberg");
 
 function luma_get_stylesheet() {
-    wp_enqueue_style('style', get_stylesheet_uri('style.css'));
-    wp_enqueue_style('bootstrap-cdn-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
-    wp_enqueue_script('bootstrap-cdn-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js');
+    wp_enqueue_style('style', get_stylesheet_directory_uri('style.css'));
+    wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css', array(), '5.3.2');
+    wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.2', true);
 }
 
 add_action('wp_enqueue_scripts', 'luma_get_stylesheet');
