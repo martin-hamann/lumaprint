3306b83c1ae4ae7cb811cd1ec3398ad194eedcc6
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-27 10:58:26 +0100

    functional navbar hopefully

--- header.php
+++ header.php
@@ -9,18 +9,22 @@
     </title>
     <?php wp_head() ?>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossorigin="anonymous"></script>
 </head>
 
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light headline">
+    <nav class="navbar navbar-expand-lg headline px-5 d-flex justify-content-between">
         <a class="navbar-brand" href="#"><span class="headline-color">LUMA</span>PRINT</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
             aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse d-flex justify-content-center id="navbarText">
-            <ul class="navbar-nav mr-auto">
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto mx-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="#">Forside</a>
                 </li>
@@ -31,7 +35,9 @@
                     <a class="nav-link" href="#">Om Os</a>
                 </li>
             </ul>
-            <button class="contact-button" type="button">Kontakt Os</button>
+            <div class="button-container">
+                <button class="contact-button btn btn-link ml-auto mr-0" type="button">Kontakt Os</button>
+            </div>
         </div>
     </nav>
 </header>
