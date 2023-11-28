10567fbb73d5d576dee6593246e3d037fe1ea8b6
mgjhub <118159119+mgjhub@users.noreply.github.com>
2023-11-28 12:03:01 +0100

    plakatprint site

---
+++ products/plakatprint.php
@@ -0,0 +1,297 @@
+<?php
+/*
+Template Name: plakatprint
+*/
+?>
+<?php get_header() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plakat Print</title>
+    <!-- Include Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Additional Styles (Optional) -->
+    <style>
+        .center-text {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+            <!-- Table 1 -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto center-text text-danger ">
+        <div class="header mb-4 py-4"> <h1>Plakat Print</h1> </div>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                    <tr>
+                    <td colspan="2" class="bg-dark text-light">200g. satin plakat print</td>
+                    </tr>
+                    <tr>
+                        <th>Størrelse</th>
+                        <th>Pris</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A4 / 210x297mm.</td>
+                        <td>15,00</td>
+                    </tr>
+                    <tr>
+                        <td>A3 / 297x420mm.</td>
+                        <td>20,00</td>
+                    </tr>
+                    <tr>
+                        <td>B3 / 350x500mm.</td>
+                        <td>50,00</td>
+                    </tr>
+                    <tr>
+                        <td>A2 / 420x594mm.</td>
+                        <td>60,00</td>
+                    </tr>
+                    <tr>
+                        <td>B2 / 500x700mm.</td>
+                        <td>75,00</td>
+                    </tr>
+                    <tr>
+                        <td>A1 / 594x841mm.</td>
+                        <td>125,00</td>
+                    </tr>
+                    <tr>
+                        <td>B1 / 700x1000mm.</td>
+                        <td>150,00</td>
+                    </tr>
+                    <tr>
+                        <td>A0 / 841x1189mm.</td>
+                        <td>200,00</td>
+                    </tr>
+                    <tr>
+                        <td>B0 / 1000x1400mm</td>
+                        <td>270,00</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="2">God til indramning. <br>
+                         Skjolder ikke mod glas.</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
+
+
+            <!-- Table 2 -->
+            <div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                    <tr>
+                    <td colspan="2" class="bg-dark text-light">200g. satin plakat print</td>
+                    </tr>
+                    <tr>
+                        <th>Størrelse</th>
+                        <th>Pris</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A4 / 210x297mm.</td>
+                        <td>20,00</td>
+                    </tr>
+                    <tr>
+                        <td>A3 / 297x420mm.</td>
+                        <td>30,00</td>
+                    </tr>
+                    <tr>
+                        <td>B3 / 350x500mm.</td>
+                        <td>75,00</td>
+                    </tr>
+                    <tr>
+                        <td>A2 / 420x594mm.</td>
+                        <td>90,00</td>
+                    </tr>
+                    <tr>
+                        <td>B2 / 500x700mm.</td>
+                        <td>113,00</td>
+                    </tr>
+                    <tr>
+                        <td>A1 / 594x841mm.</td>
+                        <td>188,00</td>
+                    </tr>
+                    <tr>
+                        <td>B1 / 700x1000mm.</td>
+                        <td>225,00</td>
+                    </tr>
+                    <tr>
+                        <td>A0 / 841x1189mm.</td>
+                        <td>300,00</td>
+                    </tr>
+                    <tr>
+                        <td>B0 / 1000x1400mm</td>
+                        <td>N/A</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="2">Printet har en 5 mm. margin. <br>
+                         med ren laminat.</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
+
+            <!-- Table 3 -->
+            <div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                    <tr>
+                    <td colspan="2" class="bg-dark text-light">200g. satin plakat print</td>
+                    </tr>
+                    <tr>
+                        <th>Størrelse</th>
+                        <th>Pris</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A4 / 210x297mm.</td>
+                        <td>45,00</td>
+                    </tr>
+                    <tr>
+                        <td>A3 / 297x420mm.</td>
+                        <td>75,00</td>
+                    </tr>
+                    <tr>
+                        <td>B3 / 350x500mm.</td>
+                        <td>99,00</td>
+                    </tr>
+                    <tr>
+                        <td>A2 / 420x594mm.</td>
+                        <td>135,00</td>
+                    </tr>
+                    <tr>
+                        <td>B2 / 500x700mm.</td>
+                        <td>150,00</td>
+                    </tr>
+                    <tr>
+                        <td>A1 / 594x841mm.</td>
+                        <td>180,00</td>
+                    </tr>
+                    <tr>
+                        <td>B1 / 700x1000mm.</td>
+                        <td>217,00</td>
+                    </tr>
+                    <tr>
+                        <td>A0 / 841x1189mm.</td>
+                        <td>315,00</td>
+                    </tr>
+                    <tr>
+                        <td>B0 / 1000x1400mm</td>
+                        <td>441,00</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                        <tr>
+                        <td colspan="2">FSC certificeret mat papir. <br>
+                         Kan indrammes.</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
+
+            <!-- Table 4 -->
+            <div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                    <tr>
+                    <td colspan="2" class="bg-dark text-light">200g. satin plakat print</td>
+                    </tr>
+                    <tr>
+                        <th>Størrelse</th>
+                        <th>Pris</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>A4 / 210x297mm.</td>
+                        <td>40,00</td>
+                    </tr>
+                    <tr>
+                        <td>A3 / 297x420mm.</td>
+                        <td>70,00</td>
+                    </tr>
+                    <tr>
+                        <td>B3 / 350x500mm.</td>
+                        <td>110,00</td>
+                    </tr>
+                    <tr>
+                        <td>A2 / 420x594mm.</td>
+                        <td>150,00</td>
+                    </tr>
+                    <tr>
+                        <td>B2 / 500x700mm.</td>
+                        <td>175,00</td>
+                    </tr>
+                    <tr>
+                        <td>A1 / 594x841mm.</td>
+                        <td>200,00</td>
+                    </tr>
+                    <tr>
+                        <td>B1 / 700x1000mm.</td>
+                        <td>263,00</td>
+                    </tr>
+                    <tr>
+                        <td>A0 / 841x1189mm.</td>
+                        <td>325,00</td>
+                    </tr>
+                    <tr>
+                        <td>B0 / 1000x1400mm</td>
+                        <td>455,00</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                        <tr>
+                        <td colspan="2">Anbefales til indramning. Det skjolder. <br>
+                         Vælg 200g satin plakat print til indramning.</td>
+                    </tr>
+                </tfoot>
+            </table>
+
+             <!-- Additional Text Below Tables -->
+             <p class="text-center mt-3 text-danger">Priser er inklusiv moms. Eksl. grafisk opsætning og fragt.</p>
+             <p class="text-center mt-3 text-danger">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud </p>
+             <div class="container d-flex justify-content-center"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
