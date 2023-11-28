ae44d95188ff3a52a980daf6b4a2d6ad16d77865
Kajmak101 <Adnan_s@live.dk>
2023-11-28 13:15:04 +0100

    galleri til kunderne

--- front-page.php
+++ front-page.php
@@ -33,4 +33,41 @@
     <p class="din-printbutik-text text-center text">Vi er din partner inden for printløsninger. Vi leverer skarpe og professionelle trykprodukter, der passer til dine behov. Fra skræddersyede designs til hurtig produktion – vi bringer dine ideer til live med kvalitetsprint.</p>
     <?php get_template_part('template-parts/how-it-works/how-it-works'); ?>
 </div>
+
+
+$kreation = new WP_Query(
+    array(
+        'post_type'      => 'kreation',
+        'posts_per_page' => -1,
+    )
+);
+
+?>
+<div class="test-template-part">
+    <h3 class="din-printbutik headline text-center">Del dine kreationer</h3>
+    <p class="din-printbutik-text text-center text">Del fotos eller videoer af dine seneste kreationer med os. Brug hashtagget #lumaprint for at deltage i fællesskabet. Lad os gøre dette sted til et galleri af uendelige muligheder. 
+Klar, parat, print! </p>
+    <?php get_template_part('template-parts/how-it-works/how-it-works'); ?>
+</div>
+
+<div class="logo-container">
+    <?php
+    $count = 0;
+    while ($kreation->have_posts()) :
+        $kreation->the_post();
+        $count++;
+        $is_middle = ($kreation->post_count % 2 !== 0 && $count === ceil($kreation->post_count / 2));
+        $logo_img_classes = 'logo-img';
+        if ($is_middle) {
+            $logo_img_classes .= ' middle-logo'; 
+        }
+        
+        
+    ?>
+        <div class="<?php echo esc_attr($logo_img_classes); ?>">
+            <img src="<?php the_field('kreation-logo'); ?>" alt="billed af kreation">
+        </div>
+    <?php endwhile; ?>
+</div>
+
 <?php get_footer(); ?>
