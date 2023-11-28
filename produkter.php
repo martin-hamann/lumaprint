b210e15bbc3b79b80d52c2d3f68c0876da03eec3
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-28 16:47:44 +0100

    products page added

---
+++ produkter.php
@@ -0,0 +1,160 @@
+<?php
+/*
+Template Name: Produkter
+*/
+
+get_header();
+
+?><div class="hiw-container">
+<div class="steps-container">
+    <h3>Hvordan fungerer det?</h3>
+    <div class="row gap-5 d-flex justify-content-center col-12">
+        <div class="card mb-3 col-md-4" style="max-width: 18rem;">
+            <div class="card-header headline">
+                <h2 class="card-headline headline">1.</h2>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Gennemsøg vores produkter og vælg lige det der passer dig. Uanset om det er
+                    plakat, print eller noget helt andet.</p>
+            </div>
+        </div>
+        <div class="card mb-3 col-md-4" style="max-width: 18rem;">
+            <div class="card-header headline ">
+                <h2 class="card-headline headline">2.</h2>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Send os en mail med dit design, produkt, størrelse, og andre detaljer du
+                    har i tankerne.</p>
+            </div>
+        </div>
+        <div class="card mb-3 col-md-4" style="max-width: 18rem;">
+            <div class="card-header headline">
+                <h2 class="card-headline headline">3.</h2>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Vi sender dig et uforpligtende tilbud, og så snart du godkender kickstarter
+                    vi produktionen </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+<div class="products-container">
+    <h3 class="headline">Alle Produkter</h3> 
+    <div class="row d-flex justify-content-center gap-4 products-row procuts-row-1" id="products-row">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-center gap-4 products-row procuts-row-1" id="products-row">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-center gap-4 products-row procuts-row-1" id="products-row">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-center gap-4 products-row procuts-row-1" id="products-row">
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="plakat.png" alt="Card image cap">
+            <div class="card-body product-card-body">
+                <h5 class="card-title">Plakatprint</h5>
+                <a href="#" class="læs-mere-text">Læs Mere</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+</div>
+</div>
