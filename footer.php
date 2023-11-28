9379e7096c03c43c9c731f2cd9407fef41568f1f
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-28 10:53:56 +0100

    First Footer Test + CSS update

--- footer.php
+++ footer.php
@@ -1,4 +1,69 @@
+</body>
+<footer id="footer" class="footer-1">
+    <div class="main-footer widgets-dark typo-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="widget subscribe no-box">
+                        <h5 class="widget-title"><span>LUMA</span>PRINT</h5>
+                        <p>Skabt til Print, Trykt til Perfektion. </p>
+                        <img src="Luma_icon_1 frilagt.png" alt="">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="widget no-box">
+                        <h5 class="widget-title">Nyttige Links</h5>
+                        <ul class="thumbnail-widget">
+                            <li>
+                                <div class="thumb-content"><a href="#.">&nbsp;Forside</a></div>
+                            </li>
+                            <li>
+                                <div class="thumb-content"><a href="#.">&nbsp;Produkter</a></div>
+                            </li>
+                            <li>
+                                <div class="thumb-content"><a href="#.">&nbsp;Om Os</a></div>
+                            </li>
+                            <li>
+                                <div class="thumb-content"><a href="#.">&nbsp;Kontakt Os</a></div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="widget no-box">
+                        <h5 class="widget-title">Følg Med</h5>
+                        <a href="#"> <i class="fa fa-facebook"> </i> </a>
+                        <a href="#"> <i class="fa fa-instagram"> </i> </a>
+                        <a href="#"> <i class="fa fa-twitter"> </i> </a>
+                    </div>
+                </div>
 
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="widget no-box">
+                        <h5 class="widget-title">Kontakt Os</h5>
+                        <p>Brug knappen nedenfor hvis du har spørgsmål eller vil have et uforpligtende tilbud</p>
+                        <div class="kontakt-os-field">
+                            <a href=""><button type="button" class="btn kontakt-os-knap">Kom i gang</button></a>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <p>Copyright LUMAPRINT aps © 2023. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php wp_footer() ?>
-    </body>
+</footer>
+
+
+
 </html>
--- header.php
+++ header.php
@@ -14,6 +14,7 @@
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/b2340e7a76.js" crossorigin="anonymous"></script>
 </head>
 
 <header>
