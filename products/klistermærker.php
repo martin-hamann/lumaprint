675d22339dbb57fddf53a4e560f6037d2062a9d1
mgjhub <118159119+mgjhub@users.noreply.github.com>
2023-11-29 11:07:46 +0100

    text update

--- products/klistermærker.php
+++ products/klistermærker.php
@@ -60,12 +60,11 @@ Template Name: Klistermærker
 
                Vejledende pris:
                75,00 for et A4 ark med klistermærker.
-               100,00 for et A3 ark med klistermærker.
-
-               Priser er inkl. moms.
-               Eksl. grafisk arbejde og fragt. <br> Lever venligst en PDF fil til print. </p>
+               100,00 for et A3 ark med klistermærker.  </p>
             
             <img src="https://images.pexels.com/photos/2783837/pexels-photo-2783837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Stickers" width= "50%">
+
+            <p>Priser er inkl. moms. Eksl. grafisk arbejde og fragt. <br> Lever venligst en PDF fil til print. </p>
         </div>
     </div>
 
