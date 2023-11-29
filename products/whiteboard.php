685ff91d0fb2b6e55e06dd53c7f1bc7184246420
mgjhub <118159119+mgjhub@users.noreply.github.com>
2023-11-29 12:28:43 +0100

    whiteboard page

---
+++ products/whiteboard.php
@@ -0,0 +1,405 @@
+<?php
+/*
+Template Name: Whiteboard
+*/
+?>
+<?php get_header() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Flyers</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .center-text {
+            text-align: center;
+            color: white;
+        }
+    </style>
+</head>
+
+<body>
+
+<!-- Table 1 -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto center-text text-danger ">
+            <div class="header "> <h1>Whiteboard med Print</h1>
+                <p> Whiteboard med dit eget design eller layout. f.eks ugeplan, arbejdsplan, pointtavle, osv.</p></div>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                <tr>
+                    <td colspan="7" class="bg-dark text-light">Whiteboard poster print <br> Ikke magnetisk. Tynd plakat print som kan rulles.</td>
+                </tr>
+                <tr>
+                    <th>Pris</th>
+                    <th>Størrelse</th>
+                    <th>MM</th>
+                    <th>Ex.Moms</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>106.09</td>
+                    <td>A5</td>
+                    <td>148x210</td>
+                    <td>84.88</td>
+                </tr>
+                <tr>
+                    <td>148.53</td>
+                    <td>B5</td>
+                    <td>176x250</td>
+                    <td>118.83</td>
+                </tr>
+                <tr>
+                    <td>151.56</td>
+                    <td>A4</td>
+                    <td>210x297</td>
+                    <td>121.25</td>
+                </tr>
+                <tr>
+                    <td>212.19</td>
+                    <td>B4</td>
+                    <td>250x350</td>
+                    <td>169,75</td>
+                </tr>
+                <tr>
+                    <td>216,52</td>
+                    <td>A3</td>
+                    <td>297x420</td>
+                    <td>173,22</td>
+                </tr>
+                <tr>
+                    <td>303,13</td>
+                    <td>B3 </td>
+                    <td>350x500</td>
+                    <td>242,50</td>
+                </tr>
+                <tr>
+                    <td>309,31</td>
+                    <td>A2</td>
+                    <td>420x594</td>
+                    <td>247,45</td>
+                </tr>
+                <tr>
+                    <td>433,04</td>
+                    <td>B2</td>
+                    <td>500x700</td>
+                    <td>346,43</td>
+                </tr>
+                <tr>
+                    <td>441,88</td>
+                    <td>A1</td>
+                    <td>594x840</td>
+                    <td>353,50</td>
+                </tr>
+                <tr>
+                    <td>618,63</td>
+                    <td>B1</td>
+                    <td>700x1000</td>
+                    <td>494,90</td>
+                </tr>
+                <tr>
+                    <td>631,25</td>
+                    <td>A0</td>
+                    <td>840x1189</td>
+                    <td>505,00</td>
+                </tr>
+                <tr>
+                    <td>883,75</td>
+                    <td>B0</td>
+                    <td>1000x1400</td>
+                    <td>707,00</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                <tr>
+                    <td colspan="7" class="bg-dark text-light">Whiteboard klæbe folie <br>Ikke magnetisk. 
+                    Klæbefolie med print til at montere på glat overflade, f.eks køleskab, dør, bord, stålplade, osv eller evt. på dit egen whiteboard tavle. Folien er nemt at montere, da den har luftkaneler på klæber siden.</td>
+                </tr>
+                <tr>
+                    <th>Pris</th>
+                    <th>Størrelse</th>
+                    <th>MM</th>
+                    <th>Ex.Moms</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>106.09</td>
+                    <td>A5</td>
+                    <td>148x210</td>
+                    <td>84.88</td>
+                </tr>
+                <tr>
+                    <td>148.53</td>
+                    <td>B5</td>
+                    <td>176x250</td>
+                    <td>118.83</td>
+                </tr>
+                <tr>
+                    <td>151.56</td>
+                    <td>A4</td>
+                    <td>210x297</td>
+                    <td>121.25</td>
+                </tr>
+                <tr>
+                    <td>212.19</td>
+                    <td>B4</td>
+                    <td>250x350</td>
+                    <td>169,75</td>
+                </tr>
+                <tr>
+                    <td>216,52</td>
+                    <td>A3</td>
+                    <td>297x420</td>
+                    <td>173,22</td>
+                </tr>
+                <tr>
+                    <td>303,13</td>
+                    <td>B3 </td>
+                    <td>350x500</td>
+                    <td>242,50</td>
+                </tr>
+                <tr>
+                    <td>309,31</td>
+                    <td>A2</td>
+                    <td>420x594</td>
+                    <td>247,45</td>
+                </tr>
+                <tr>
+                    <td>433,04</td>
+                    <td>B2</td>
+                    <td>500x700</td>
+                    <td>346,43</td>
+                </tr>
+                <tr>
+                    <td>441,88</td>
+                    <td>A1</td>
+                    <td>594x840</td>
+                    <td>353,50</td>
+                </tr>
+                <tr>
+                    <td>618,63</td>
+                    <td>B1</td>
+                    <td>700x1000</td>
+                    <td>494,90</td>
+                </tr>
+                <tr>
+                    <td>631,25</td>
+                    <td>A0</td>
+                    <td>840x1189</td>
+                    <td>505,00</td>
+                </tr>
+                <tr>
+                    <td>883,75</td>
+                    <td>B0</td>
+                    <td>1000x1400</td>
+                    <td>707,00</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                <tr>
+                    <td colspan="7" class="bg-dark text-light">Whiteboard på magnetisk folie <br>
+                    0,5 mm. tyk magnetisk folie med print. Kan sættes på metal eller stål flader. Evt. rulles sammen til turen.</td>
+                </tr>
+                <tr>
+                    <th>Pris</th>
+                    <th>Størrelse</th>
+                    <th>MM</th>
+                    <th>Ex.Moms</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>543.75</td>
+                    <td>A5</td>
+                    <td>148x210</td>
+                    <td>435.00</td>
+                </tr>
+                <tr>
+                    <td>552.50</td>
+                    <td>B5</td>
+                    <td>176x250</td>
+                    <td>442.00</td>
+                </tr>
+                <tr>
+                    <td>556.00</td>
+                    <td>A4</td>
+                    <td>210x297</td>
+                    <td>452.00</td>
+                </tr>
+                <tr>
+                    <td>581.25</td>
+                    <td>B4</td>
+                    <td>250x350</td>
+                    <td>465.00</td>
+                </tr>
+                <tr>
+                    <td>605.00</td>
+                    <td>A3</td>
+                    <td>297x420</td>
+                    <td>484.00</td>
+                </tr>
+                <tr>
+                    <td>637.50</td>
+                    <td>B3 </td>
+                    <td>350x500</td>
+                    <td>510.00</td>
+                </tr>
+                <tr>
+                    <td>687.50</td>
+                    <td>A2</td>
+                    <td>420x594</td>
+                    <td>550.00</td>
+                </tr>
+                <tr>
+                    <td>753.75</td>
+                    <td>B2</td>
+                    <td>500x700</td>
+                    <td>603.00</td>
+                </tr>
+                <tr>
+                    <td>853.75</td>
+                    <td>A1</td>
+                    <td>594x840</td>
+                    <td>683.00</td>
+                </tr>
+                <tr>
+                    <td>982.50</td>
+                    <td>B1</td>
+                    <td>700x1000</td>
+                    <td>786.00</td>
+                </tr>
+                <tr>
+                    <td>1183.75</td>
+                    <td>A0</td>
+                    <td>840x1189</td>
+                    <td>947.00</td>
+                </tr>
+                <tr>
+                    <td>1443.75</td>
+                    <td>B0</td>
+                    <td>1000x1400</td>
+                    <td>1155.00</td>
+                </tr>
+                </tbody>
+            </table>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-4">
+                <thead>
+                <tr>
+                    <td colspan="7" class="bg-dark text-light"> Whiteboard plade print <br>
+                    Ikke magnetisk. 3 mm. tyk plade.</td>
+                </tr>
+                <tr>
+                    <th>Pris</th>
+                    <th>Størrelse</th>
+                    <th>MM</th>
+                    <th>Ex.Moms</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>543.75</td>
+                    <td>A5</td>
+                    <td>148x210</td>
+                    <td>435.00</td>
+                </tr>
+                <tr>
+                    <td>552.50</td>
+                    <td>B5</td>
+                    <td>176x250</td>
+                    <td>442.00</td>
+                </tr>
+                <tr>
+                    <td>556.00</td>
+                    <td>A4</td>
+                    <td>210x297</td>
+                    <td>452.00</td>
+                </tr>
+                <tr>
+                    <td>581.25</td>
+                    <td>B4</td>
+                    <td>250x350</td>
+                    <td>465.00</td>
+                </tr>
+                <tr>
+                    <td>605.00</td>
+                    <td>A3</td>
+                    <td>297x420</td>
+                    <td>484.00</td>
+                </tr>
+                <tr>
+                    <td>637.50</td>
+                    <td>B3 </td>
+                    <td>350x500</td>
+                    <td>510.00</td>
+                </tr>
+                <tr>
+                    <td>687.50</td>
+                    <td>A2</td>
+                    <td>420x594</td>
+                    <td>550.00</td>
+                </tr>
+                <tr>
+                    <td>753.75</td>
+                    <td>B2</td>
+                    <td>500x700</td>
+                    <td>603.00</td>
+                </tr>
+                <tr>
+                    <td>853.75</td>
+                    <td>A1</td>
+                    <td>594x840</td>
+                    <td>683.00</td>
+                </tr>
+                <tr>
+                    <td>982.50</td>
+                    <td>B1</td>
+                    <td>700x1000</td>
+                    <td>786.00</td>
+                </tr>
+                <tr>
+                    <td>1183.75</td>
+                    <td>A0</td>
+                    <td>840x1189</td>
+                    <td>947.00</td>
+                </tr>
+                <tr>
+                    <td>1443.75</td>
+                    <td>B0</td>
+                    <td>1000x1400</td>
+                    <td>1155.00</td>
+                </tr>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="center-text">
+    <p>Priser er inklusiv moms. Eksl. print og fragt. <br> Lever venligst en PDF fil til print.</p>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+
+</html>
