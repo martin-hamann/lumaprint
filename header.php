50b4b3285f1f46521ccad32aeca07bf9363fe73f
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-29 13:22:48 +0100

    trying navbar fix

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
     </title>
     <?php wp_head() ?>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css">
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script defer src="https://kit.fontawesome.com/b2340e7a76.js" crossorigin="anonymous"></script>
 </head>
@@ -17,11 +16,11 @@
 <header>
     <nav class="navbar navbar-expand-lg headline px-5 d-flex justify-content-between">
         <a class="navbar-brand" href="http://lumaprint.site/"><span class="headline-color">LUMA</span>PRINT</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
+            aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse" id="navbar">
             <ul class="navbar-nav mr-auto mx-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="http://lumaprint.site/">Forside</a>
