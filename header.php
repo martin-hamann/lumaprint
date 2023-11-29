9d212b9286c86d8f9c6ac8ccb42356c02ee9ee0b
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-29 13:50:20 +0100

    test with temp navbar

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
     <script defer src="https://kit.fontawesome.com/b2340e7a76.js" crossorigin="anonymous"></script>
 </head>
 
-<nav class="navbar navbar-expand-lg headline px-5 d-flex justify-content-between">
+<!-- <nav class="navbar navbar-expand-lg headline px-5 d-flex justify-content-between">
     <a class="navbar-brand" href="http://lumaprint.site/"><span class="headline-color">LUMA</span>PRINT</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
         aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,6 +37,21 @@
                     class="contact-button btn btn-link ml-auto mr-0" type="button">Kontakt Os</button></a>
         </div>
     </div>
+</nav> -->
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+  </div>
 </nav>
-
 <body>
