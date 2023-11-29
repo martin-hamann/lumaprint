e3636c8c1fd834e43cb9fd96caddc08408a11592
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-29 11:48:47 +0100

    google reviews update

--- front-page.php
+++ front-page.php
@@ -13,8 +13,8 @@
 <div class="container col-xxl-8 px-4 py-5 hero-container">
     <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div class="col-10 col-sm-8 col-lg-6">
-            <img src="http://lumaprint.site/wp-content/uploads/2023/11/printer.png" class="d-block mx-lg-auto img-fluid" alt="FuturisticPrinter" width="700" height="500"
-                loading="lazy">
+            <img src="http://lumaprint.site/wp-content/uploads/2023/11/printer.png" class="d-block mx-lg-auto img-fluid"
+                alt="FuturisticPrinter" width="700" height="500" loading="lazy">
         </div>
         <div class="col-lg-6">
             <h1 class="display-7 fw-bold lh-1 mb-3 hero-headline">LA' OS PRINTE DET FOR DIG</h1>
@@ -30,39 +30,42 @@
 
 <div class="test-template-part">
     <h3 class="din-printbutik headline text-center">Din Printbutik I Hjertet Af Esbjerg</h3>
-    <p class="din-printbutik-text text-center text">Vi er din partner inden for printløsninger. Vi leverer skarpe og professionelle trykprodukter, der passer til dine behov. Fra skræddersyede designs til hurtig produktion – vi bringer dine ideer til live med kvalitetsprint.</p>
+    <p class="din-printbutik-text text-center text">Vi er din partner inden for printløsninger. Vi leverer skarpe og
+        professionelle trykprodukter, der passer til dine behov. Fra skræddersyede designs til hurtig produktion – vi
+        bringer dine ideer til live med kvalitetsprint.</p>
     <?php get_template_part('template-parts/how-it-works/how-it-works'); ?>
 </div>
 
 <?php
 $kreation = new WP_Query(
     array(
-        'post_type'      => 'kreation',
+        'post_type' => 'kreation',
         'posts_per_page' => -1,
     )
 );
 
 ?>
 
-    <h3 class="din-printbutik headline text-center">Del Dine Kreationer</h3>
-    <p class="din-printbutik-text text-center text">Del fotos eller videoer af dine seneste kreationer med os. Brug hashtagget #lumaprint for at deltage i fællesskabet. Lad os gøre dette sted til et galleri af uendelige muligheder. 
-Klar, parat, print! </p>
+<h3 class="din-printbutik headline text-center">Del Dine Kreationer</h3>
+<p class="din-printbutik-text text-center text">Del fotos eller videoer af dine seneste kreationer med os. Brug
+    hashtagget #lumaprint for at deltage i fællesskabet. Lad os gøre dette sted til et galleri af uendelige muligheder.
+    Klar, parat, print! </p>
 
 
 <div class="logo-container">
     <?php
     $count = 0;
-    while ($kreation->have_posts()) :
+    while ($kreation->have_posts()):
         $kreation->the_post();
         $count++;
         $is_middle = ($kreation->post_count % 2 !== 0 && $count === ceil($kreation->post_count / 2));
         $logo_img_classes = 'logo-img';
         if ($is_middle) {
-            $logo_img_classes .= ' middle-logo'; 
+            $logo_img_classes .= ' middle-logo';
         }
-        
-        
-    ?>
+
+
+        ?>
         <div class="<?php echo esc_attr($logo_img_classes); ?>">
             <img src="<?php the_field('kreation-logo'); ?>" alt="billed af kreation">
         </div>
@@ -70,7 +73,11 @@ Klar, parat, print! </p>
 </div>
 
 
-<?php echo do_shortcode( '[trustindex no-registration=google]' ); ?>
+<div class="google-reviews-container>
+
+<?php echo do_shortcode('[trustindex no-registration=google]'); ?>
+
+</div>
 
 
 <?php get_footer(); ?>
