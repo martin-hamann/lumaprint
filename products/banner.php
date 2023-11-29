dc376ac878a1123a2ab60bec99a6c17dbc74cb00
martin-hamann <martinhamannnielsen@gmail.com>
2023-11-29 12:29:21 +0100

    Update banner.php

--- products/banner.php
+++ products/banner.php
@@ -8,118 +8,133 @@ Template Name: Banner
 
 <div class="container">
     <div class="header mb-4 py-4">
-        <h2 class="banner-headline headline">Banner</h2>
+        <h2 class="banner-headline headline">Bannerprint</h2>
     </div>
     <div class="row image-container d-flex justify-content-center">
         <div class="col-md-3 image-box">
-            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Vinyl.jpg" alt="Vinyl">
-            <h4 class="Headline">Vinyl banner</h4>
-            <p class="banner-text">Outdoor eller indoor B1 pressing banner.
+            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Vinyl.jpg" alt="Vinyl" class="banner-img">
+            <h4 class="banner-sub-headline headline">Banner af Vinyl</h4>
+            <p class="banner-text text-center">Outdoor eller indoor B1 pressing banner.
                 Print på en side.</p>
         </div>
         <div class="col-md-3 image-box">
-            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mesh.jpg" alt="Mesh">
-            <h4 class="Headline">Mesh banner</h4>
-            <p class="banner-text">Outdoor banner med små huller som tillader vinden at trænge igennem. Det er velegnet
+            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mesh.jpg" alt="Mesh" class="banner-img">
+            <h4 class="banner-sub-headline headline">Banner af Mesh</h4>
+            <p class="banner-text text-center">Outdoor banner med små huller som tillader vinden at trænge igennem. Det
+                er velegnet
                 til frithængende bannere, f.eks på byggepladshegn, festivalshegn, stillads osv.
                 Print på en side.</p>
         </div>
         <div class="col-md-3 image-box">
-            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Stof.jpg" alt="Stof">
-            <h4 class="banner-sub-headline headline">Stof banner</h4>
-            <p class="banner-text">Outdoor og indoor B1 brændhæmmende fleksibel tekstil banner. Kan foldes sammen til
+            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Stof.jpg" alt="Stof" class="banner-img">
+            <h4 class="banner-sub-headline headline">Banner af Stof</h4>
+            <p class="banner-text text-center">Outdoor og indoor B1 brændhæmmende fleksibel tekstil banner. Kan foldes
+                sammen til
                 rejsetaske som er nemmere end en rullet poster.
                 Print på en side.</p>
         </div>
     </div>
-</div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 mx-auto center-text text-danger ">
-                <table class="table table-bordered border-dark text-center mb-">
-                    <thead>
 
-                        <tr>
-                            <th>Pris</th>
-                            <th>Mål i cm</th>
-                            <th>Ex.moms </th>
+    <div class="row table-container">
+        <div class="col-md-6 mx-auto center-text">
+            <table class="table table-bordered border-dark text-center">
+                <thead class="bg-dark text-light">
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>218,75</td>
-                            <td>A1/ 60x84</td>
-                            <td>175,00</td>
-                        </tr>
-                        <tr>
-                            <td>250,00</td>
-                            <td>B1/ 70x100</td>
-                            <td>200,00</td>
-                        </tr>
-                        <tr>
-                            <td>343,73</td>
-                            <td>A0/ 84x119</td>
-                            <td>275,00</td>
+                    <tr>
+                        <th>Pris</th>
+                        <th>Mål i cm</th>
+                        <th>Ex.moms </th>
 
-                        </tr>
-                        <tr>
-                            <td>418,75</td>
-                            <td>150x50 </td>
-                            <td>335,00</td>
-                        </tr>
-                        <tr>
-                            <td>576,25</td>
-                            <td>B0/ 100x140 </td>
-                            <td>461,00</td>
-                        </tr>
-                        <tr>
-                            <td>601,25 </td>
-                            <td>150x100</td>
-                            <td>481,00</td>
-                        </tr>
-                        <tr>
-                            <td>782,50</td>
-                            <td>150x150</td>
-                            <td>626,00</td>
-                        </tr>
-                        <tr>
-                            <td>965,00</td>
-                            <td>150x200</td>
-                            <td>772,00</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>218,75</td>
+                        <td>A1/ 60x84</td>
+                        <td>175,00</td>
+                    </tr>
+                    <tr>
+                        <td>250,00</td>
+                        <td>B1/ 70x100</td>
+                        <td>200,00</td>
+                    </tr>
+                    <tr>
+                        <td>343,73</td>
+                        <td>A0/ 84x119</td>
+                        <td>275,00</td>
 
-                        </tr>
-                        <tr>
-                            <td>1147,50</td>
-                            <td>150x250</td>
-                            <td>918,00</td>
-                        </tr>
-                        <tr>
-                            <td>1328,75</td>
-                            <td>150x300 </td>
-                            <td>1063,00</td>
-                        </tr>
-                    </tbody>
+                    </tr>
+                    <tr>
+                        <td>418,75</td>
+                        <td>150x50 </td>
+                        <td>335,00</td>
+                    </tr>
+                    <tr>
+                        <td>576,25</td>
+                        <td>B0/ 100x140 </td>
+                        <td>461,00</td>
+                    </tr>
+                    <tr>
+                        <td>601,25 </td>
+                        <td>150x100</td>
+                        <td>481,00</td>
+                    </tr>
+                    <tr>
+                        <td>782,50</td>
+                        <td>150x150</td>
+                        <td>626,00</td>
+                    </tr>
+                    <tr>
+                        <td>965,00</td>
+                        <td>150x200</td>
+                        <td>772,00</td>
 
-                </table>
-            </div>
+                    </tr>
+                    <tr>
+                        <td>1147,50</td>
+                        <td>150x250</td>
+                        <td>918,00</td>
+                    </tr>
+                    <tr>
+                        <td>1328,75</td>
+                        <td>150x300 </td>
+                        <td>1063,00</td>
+                    </tr>
+                </tbody>
+
+            </table>
         </div>
     </div>
 
-    <style>
+</div>
+<style>
     .center-text {
         text-align: center;
     }
 
     .banner-text {
         color: white;
+
     }
 
-    h2.banner.headline{
-        width:50%;
-        margin:0 auto;
+    h2.banner.headline {
+        width: 50%;
+        margin: 0 auto;
     }
-    
 
+    .banner-img {
+        object-fit: cover;
+        height: 100%;
+        width: 100%;
+    }
+    div.row .image-container{
+        margin-bottom:20vh;
+    }
+    div.row .table-container{
+        margin-bottom:20vh;
+    }
 </style>
+
+
+<?php get_footer() ?>
