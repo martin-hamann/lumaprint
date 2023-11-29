73d027e304533d703d3e86a49358b44602a41e90
Kajmak101 <Adnan_s@live.dk>
2023-11-29 11:31:01 +0100

    update banner

---
+++ products/banner.php
@@ -0,0 +1,117 @@
+<?php
+/*
+Template Name: Banner
+*/
+?>
+<?php get_header() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<div class="container">
+        <div class="row image-container">
+            <div class="col-md-3 image-box">
+                <img src="http://lumaprint.site/wp-content/uploads/2023/11/Vinyl.jpg" alt="Vinyl">
+                <h4>Vinyl banner</h4>
+                <p>Outdoor eller indoor B1 pressing banner.
+                        Print på en side.</p>
+            </div>
+            <div class="col-md-3 image-box">
+                <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mesh.jpg" alt="Mesh">
+                <h4>Mesh banner</h4>
+                <p>Outdoor banner med små huller som tillader vinden at trænge igennem. Det er velegnet til frithængende bannere, f.eks på byggepladshegn, festivalshegn, stillads osv.
+                    Print på en side.</p>
+            </div>
+            <div class="col-md-3 image-box">
+                <img src="http://lumaprint.site/wp-content/uploads/2023/11/Stof.jpg" alt="Stof">
+                <h4>Stof banner</h4>
+                <p>Outdoor og indoor B1 brændhæmmende fleksibel tekstil banner. Kan foldes sammen til rejsetaske som er nemmere end en rullet poster.
+                    Print på en side.</p>
+            </div>
+        </div>
+    </div>
+
+    <style>
+        .center-text {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+            <!-- Table 1 -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 mx-auto center-text text-danger ">
+        <div class="header mb-4 py-4"> <h1>Plakat Print</h1> </div>
+
+            <!-- Main Table -->
+            <table class="table table-bordered border-dark text-center mb-">
+                <thead>
+                   
+                    <tr>
+                        <th>Pris</th>
+                        <th>Mål i cm</th>
+                        <th>Ex.moms </th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>218,75</td>
+                        <td>A1/ 60x84</td>
+                        <td>175,00</td>
+                    </tr>
+                    <tr>
+                        <td>250,00</td>
+                        <td>B1/ 70x100</td>
+                        <td>200,00</td>
+                    </tr>
+                    <tr>
+                        <td>343,73</td>
+                        <td>A0/ 84x119</td>
+                        <td>275,00</td>
+                        
+                    </tr>
+                    <tr>
+                        <td>418,75</td>
+                        <td>150x50	</td>
+                        <td>335,00</td>
+                    </tr>
+                    <tr>
+                        <td>576,25</td>
+                        <td>B0/ 100x140	</td>
+                        <td>461,00</td>
+                    </tr>
+                    <tr>
+                        <td>601,25	</td>
+                        <td>150x100</td>
+                        <td>481,00</td>
+                    </tr>
+                    <tr>
+                        <td>782,50</td>
+                        <td>150x150</td>
+                        <td>626,00</td>
+                    </tr>
+                    <tr>
+                        <td>965,00</td>
+                        <td>150x200</td>
+                        <td>772,00</td>
+
+                    </tr>
+                    <tr>
+                        <td>1147,50</td>
+                        <td>150x250</td>
+                        <td>918,00</td>
+                    </tr>
+                    <tr>
+                        <td>1328,75</td>
+                        <td>150x300	</td>
+                        <td>1063,00</td>
+                    </tr>
+                </tbody>
+               
+            </table>
+        </div>
+    </div>
+</div>
