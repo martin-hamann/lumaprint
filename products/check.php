9f68e64c64ad1dd08dfd62ba2dcf09e5e60477c4
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-01 00:54:43 +0100

    Update check.php

--- products/check.php
+++ products/check.php
@@ -12,11 +12,6 @@ Template Name: check
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Check Page</title>
     <style>
-
-        body  {
-            color: white;
-        }
-
         #image-container {
             text-align: center;
             margin-bottom: 20px;
@@ -27,13 +22,13 @@ Template Name: check
             height: auto;
         }
 
-        #text {
+        .text-container {
             text-align: center;
             width: 50%;
             margin: 0 auto;
             
         }
-        .kontakt-os-knap{
+        .check-kontakt-os-knap{
             margin-bottom:15vh;
         }
     </style>
@@ -46,7 +41,7 @@ Template Name: check
         <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-billeder.jpg" alt="check/gavekort">
     </div>
 
-    <div id="text-container">
+    <div class="text-container text-center">
         <p class="text">Et 44x100 cm. farveprint klæbet på en 5 mm. tyk pur-skumplade. Farven kan efterligne dit logo eller som du ønsker. Mail os dit logo og info, så mailer vi en korrektur tilbage med et par layout forslag. Eller mail din egen design/layout som PDF fil til os.
         250,00 eksl. fragt. Afhentning hos Lumaprint Esbjerg.
         350,00 inkl. fragt til en dansk adresse.
@@ -59,7 +54,7 @@ Template Name: check
             <a href="#">Eksempler</a></p> -->
     </div>
     <p class="text-center mt-3 text">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud <br> Lever venligst en PDF fil til print. </p>
-    <div class="container d-flex justify-content-center kontakt-os-knap"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
+    <div class="container d-flex justify-content-center check-kontakt-os-knap"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
 </body>
 </html>
 
