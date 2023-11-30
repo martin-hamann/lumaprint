97c04b552133cb42d8eccc89bde27f84273115d9
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-01 00:52:38 +0100

    Update check.php

--- products/check.php
+++ products/check.php
@@ -33,30 +33,33 @@ Template Name: check
             margin: 0 auto;
             
         }
+        .kontakt-os-knap{
+            margin-bottom:15vh;
+        }
     </style>
 </head>
 <body>
 
-    <header style ="text-align: center; font-size: 24px;"> Stor Check/Gavekort</header>
+    <h2 class="headline check-headline">Stor Check/Gavekort</h2>
 
     <div id="image-container">
         <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-billeder.jpg" alt="check/gavekort">
     </div>
 
-    <div id="text">
-        <p>Et 44x100 cm. farveprint klæbet på en 5 mm. tyk pur-skumplade. Farven kan efterligne dit logo eller som du ønsker. Mail os dit logo og info, så mailer vi en korrektur tilbage med et par layout forslag. Eller mail din egen design/layout som PDF fil til os.
+    <div id="text-container">
+        <p class="text">Et 44x100 cm. farveprint klæbet på en 5 mm. tyk pur-skumplade. Farven kan efterligne dit logo eller som du ønsker. Mail os dit logo og info, så mailer vi en korrektur tilbage med et par layout forslag. Eller mail din egen design/layout som PDF fil til os.
         250,00 eksl. fragt. Afhentning hos Lumaprint Esbjerg.
         350,00 inkl. fragt til en dansk adresse.
         Priser er inkl. moms.
 
         Kan evt. bruges som gavekort.
         For andre størrelser, venligst mail efter en pris.</p>
-        <p> Skabeloner: <a href="#">Powerpoint</a>
+        <!-- <p> Skabeloner: <a href="#">Powerpoint</a>
             <a href="#">Publisher</a>
-            <a href="#">Eksempler</a></p>
+            <a href="#">Eksempler</a></p> -->
     </div>
-    <p class="text-center mt-3">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud <br> Lever venligst en PDF fil til print. </p>
-    <div class="container d-flex justify-content-center"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
+    <p class="text-center mt-3 text">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud <br> Lever venligst en PDF fil til print. </p>
+    <div class="container d-flex justify-content-center kontakt-os-knap"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
 </body>
 </html>
 
