c0c84de2ada7652830c610e22eb8bed3dcb2cbc9
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-01 00:58:45 +0100

    Update check.php

--- products/check.php
+++ products/check.php
@@ -7,11 +7,16 @@ Template Name: check
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your Check Page</title>
     <style>
+
+        .check-container{
+            margin:10vh 0;
+        }
         #image-container {
             text-align: center;
             margin-bottom: 20px;
@@ -26,36 +31,41 @@ Template Name: check
             text-align: center;
             width: 50%;
             margin: 0 auto;
-            
+
         }
-        .check-kontakt-os-knap{
-            margin-bottom:15vh;
-        }
+
     </style>
 </head>
+
 <body>
+    <div class="check-container container">
+        <h2 class="headline check-headline">Stor Check/Gavekort</h2>
 
-    <h2 class="headline check-headline">Stor Check/Gavekort</h2>
+        <div id="image-container">
+            <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-billeder.jpg" alt="check/gavekort">
+        </div>
 
-    <div id="image-container">
-        <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-billeder.jpg" alt="check/gavekort">
-    </div>
+        <div class="text-container text-center">
+            <p class="text">Et 44x100 cm. farveprint klæbet på en 5 mm. tyk pur-skumplade. Farven kan efterligne dit
+                logo eller som du ønsker. Mail os dit logo og info, så mailer vi en korrektur tilbage med et par layout
+                forslag. Eller mail din egen design/layout som PDF fil til os.
+                250,00 eksl. fragt. Afhentning hos Lumaprint Esbjerg.
+                350,00 inkl. fragt til en dansk adresse.
+                Priser er inkl. moms.
 
-    <div class="text-container text-center">
-        <p class="text">Et 44x100 cm. farveprint klæbet på en 5 mm. tyk pur-skumplade. Farven kan efterligne dit logo eller som du ønsker. Mail os dit logo og info, så mailer vi en korrektur tilbage med et par layout forslag. Eller mail din egen design/layout som PDF fil til os.
-        250,00 eksl. fragt. Afhentning hos Lumaprint Esbjerg.
-        350,00 inkl. fragt til en dansk adresse.
-        Priser er inkl. moms.
-
-        Kan evt. bruges som gavekort.
-        For andre størrelser, venligst mail efter en pris.</p>
-        <!-- <p> Skabeloner: <a href="#">Powerpoint</a>
+                Kan evt. bruges som gavekort.
+                For andre størrelser, venligst mail efter en pris.</p>
+            <!-- <p> Skabeloner: <a href="#">Powerpoint</a>
             <a href="#">Publisher</a>
             <a href="#">Eksempler</a></p> -->
+        </div>
+        <p class="text-center mt-3 text">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud <br>
+            Lever venligst en PDF fil til print. </p>
+        <div class="container d-flex justify-content-center"><button type="button"
+                class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
     </div>
-    <p class="text-center mt-3 text">Lyder det som noget for dig så kontakt os for et uforpligtende tilbud <br> Lever venligst en PDF fil til print. </p>
-    <div class="container d-flex justify-content-center check-kontakt-os-knap"><button type="button" class="btn btn-lg px-4 me-md-2 hero-button-1 ">Kontakt Os</button> </div>
 </body>
+
 </html>
 
 <?php get_footer(); ?>
