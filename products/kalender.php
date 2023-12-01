d157f40f520fc55280b8f45e2b8fbdb0be9d6d50
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-01 02:05:38 +0100

    Update kalender.php

--- products/kalender.php
+++ products/kalender.php
@@ -36,32 +36,32 @@ Template Name: Kalender
         <div class="row col-md-12">
             <div class="box col-md-4">
                 <h4 class="headline">A5 Bordkalender</h4>
-                <p> Bordkalender med farve print </p>
-                <p>1 stk for 16kr</p>
-                <p>10 stk for 123kr</p>
-                <p>25 stk for 291kr</p>
-                <p>50 stk for 472kr </p>
-                <p>100 stk for 788kr</p>
+                <p class="text"> Bordkalender med farve print </p>
+                <p class="text">1 stk for 16kr</p>
+                <p class="text">10 stk for 123kr</p>
+                <p class="text">25 stk for 291kr</p>
+                <p class="text">50 stk for 472kr </p>
+                <p class="text">100 stk for 788kr</p>
             </div>
 
             <div class="box col-md-4">
                 <h4 class="headline">A4 Kalender</h4>
-                <p>281x48 mm. felt til logo, info og reklame.</p>
-                <p>1 stk for 22kr</p>
-                <p>10 stk for 116kr</p>
-                <p>25 stk for 285kr</p>
-                <p>50 stk for 516kr </p>
-                <p>100 stk for 844kr</p>
+                <p class="text">281x48 mm. felt til logo, info og reklame.</p>
+                <p class="text">1 stk for 22kr</p>
+                <p class="text">10 stk for 116kr</p>
+                <p class="text">25 stk for 285kr</p>
+                <p class="text">50 stk for 516kr </p>
+                <p class="text">100 stk for 844kr</p>
             </div>
         </div>
 
-        <div class="row col-md-12">
+        <div class="row col-md-12 d-flex flex-wrap">
             <div class="box col-md-4">
                 <h3 class="headline">Spiral Kalender</h3>
-                <p>Spiral kalender med 1 eller 2 fotos
+                <p class="text">Spiral kalender med 1 eller 2 fotos
                     pr. måned.</p>
-                <p>A4 str for 76kr</p>
-                <p>A3 str for 135kr</p>
+                <p class="text">A4 str for 76kr</p>
+                <p class="text">A3 str for 135kr</p>
             </div>
             <div class="text-container box text-box col-md-4">
                 <p class="text">Priser er inklusiv moms. Eksl. print og fragt. <br> Lever venligst en PDF fil til print.
