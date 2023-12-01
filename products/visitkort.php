33d730113a3101c982fd61e1fa3f8f9acab9e0cf
Kajmak101 <Adnan_s@live.dk>
2023-12-01 09:32:00 +0100

    visitkort

---
+++ products/visitkort.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Template Name: Visitkort
+*/
+
+get_header();
+?>
+
+<style>
+
+.visit-h1 {
+    color: #e31e26;
+    font-size: 5em;
+    text-align: center;
+    margin-bottom: 150px;
+}
+
+.price-box {
+    border: 1px solid #ddd;
+    padding: 15px;
+    margin-bottom: 15px;
+    background-color: #fff; 
+}
+
+.Post-p {
+    color: #fff;
+    padding-bottom: 15px;
+}
+
+.visit-img {
+max-width: 100%;
+height: auto;
+display: block;
+margin: auto;
+margin-top: 20px;
+}
+</style>
+
+<h1 class="visit-h1">Visitkort</h1>
+
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-md-6">
+      <p class="Post-p">Dankort str. 55x85 mm.</p>
+      <p class="Post-p">Print mode info:</p>
+      <p class="Post-p">4+0 er print på kortets ene side. Enkelt sidet print.</p>
+      <p class="Post-p">4+4 er print på kortets begge sider, Dobbelt sidet print.</p>
+    </div>
+
+    <div class="col-md-6">
+      <p class="Post-p">Visitkort af plastik, for god holdbarhed.</p>
+      <p class="Post-p">Vandfast print på slidstærk materiale.</p>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-6">
+      <div class="price-box">
+        <h4>4+0 enkelt sidet alm. visitkort</h4>
+        <p>100,00 for en æske med 100 stk. 4+0 kort</p>
+        <p>178,00 for en æske med 200 stk. 4+0 kort</p>
+      </div>
+
+      <div class="price-box">
+        <h4>4+4 dobbelt sidet alm. visitkort</h4>
+        <p>168,00 for en æske med 100 stk. 4+4 kort</p>
+        <p>272,00 for en æske med 200 stk. 4+4 kort</p>
+      </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="price-box">
+        <h4>4+0 enkelt sidet plast visitkort</h4> 
+        <p>182,00 for en æske med 100 stk. 4+0 kort</p>
+        <p>308,00 for en æske med 200 stk. 4+0 kort</p>
+      </div>
+
+      <div class="price-box">
+        <h4>4+4 dobbelt sidet plast visitkort</h4>
+        <p>232,00 for en æske med 100 stk. 4+4 kort</p>
+        <p>402,00 for en æske med 200 stk. 4+4 kort</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container mt-5 align-items-center">
+  <img src="http://busters-verden.local/wp-content/uploads/2023/12/Visitkort_ijqojnim.jpg" alt="Responsive Image" class="visit-img">
+</div>
+
+<?php get_footer(); ?>
