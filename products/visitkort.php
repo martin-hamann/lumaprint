2d9cf7bcd76826d96036425b9c861787c0f2786e
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-01 12:30:09 +0100

    Update visitkort.php

--- products/visitkort.php
+++ products/visitkort.php
@@ -20,6 +20,7 @@ get_header();
     margin-bottom: 15px;
     background-color: #fff;
   }
+
   .visit-img {
     max-width: 100%;
     height: auto;
@@ -40,7 +41,7 @@ get_header();
     </div>
   </div>
   <div class="row">
-    <div class="col-md-6 text-center">
+    <div class="col-md-3 text-center">
       <p class="Post-p">4+0 er print på kortets ene side. Enkelt sidet print.</p>
       <p class="Post-p">4+4 er print på kortets begge sider, Dobbelt sidet print.</p>
     </div>
@@ -52,38 +53,41 @@ get_header();
   </div>
 
   <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-3">
       <div class="price-box">
         <h4>4+0 enkelt sidet alm. visitkort</h4>
         <p>100,00 for en æske med 100 stk. 4+0 kort</p>
         <p>178,00 for en æske med 200 stk. 4+0 kort</p>
       </div>
+    </div>
+    <div class="col-md-3">
+      <div class="price-box">
+        <h4>4+0 enkelt sidet plast visitkort</h4>
+        <p>182,00 for en æske med 100 stk. 4+0 kort</p>
+        <p>308,00 for en æske med 200 stk. 4+0 kort</p>
+      </div>
+    </div>
+  </div>
 
+  <div class="col-md-3">
+    <div class="price-box">
+      <h4>4+4 dobbelt sidet plast visitkort</h4>
+      <p>232,00 for en æske med 100 stk. 4+4 kort</p>
+      <p>402,00 for en æske med 200 stk. 4+4 kort</p>
+    </div>
+    <div class="col-md-3">
       <div class="price-box">
         <h4>4+4 dobbelt sidet alm. visitkort</h4>
         <p>168,00 for en æske med 100 stk. 4+4 kort</p>
         <p>272,00 for en æske med 200 stk. 4+4 kort</p>
       </div>
     </div>
-
-    <div class="col-md-6">
-      <div class="price-box">
-        <h4>4+0 enkelt sidet plast visitkort</h4>
-        <p>182,00 for en æske med 100 stk. 4+0 kort</p>
-        <p>308,00 for en æske med 200 stk. 4+0 kort</p>
-      </div>
-
-      <div class="price-box">
-        <h4>4+4 dobbelt sidet plast visitkort</h4>
-        <p>232,00 for en æske med 100 stk. 4+4 kort</p>
-        <p>402,00 for en æske med 200 stk. 4+4 kort</p>
-      </div>
-    </div>
   </div>
-  <div class="container mt-5 align-items-center">
-    <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-visitkort-e1701218825431.png" alt="Responsive Image"
-      class="visit-img">
-  </div>
+</div>
+<div class="container mt-5 align-items-center">
+  <img src="http://lumaprint.site/wp-content/uploads/2023/11/Mini-visitkort-e1701218825431.png" alt="Responsive Image"
+    class="visit-img">
+</div>
 </div>
 
 
