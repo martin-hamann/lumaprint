ab4896f23f660f3bb17d22930738e691600d4e1c
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-03 13:46:27 +0100

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="<?php bloginfo("encoding") ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?php bloginfo("LUMAPRINT") ?>
+        <?php echo get_the_title() ?>
     </title>
     <?php wp_head() ?>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css">
