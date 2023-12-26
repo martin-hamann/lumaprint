0571e9b5b677aab4d4d0ba986654ce52e161a0ec
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-26 12:36:07 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 function enqueue_styles(){
     wp_enqueue_style('bootstrap-cdn-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
-    wp_enqueue_style('style', get_stylesheet_directory_uri());
+    wp_enqueue_style('style', get_stylesheet_directory_uri() , '/style.css');
     wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
 }
 
