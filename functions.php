3fcb22829a2a60bd4d537b9434dc863bb7f5ec77
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-26 12:24:57 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,15 @@
-<?php function luma_get_stylesheet() {
-    wp_enqueue_style('style', get_stylesheet_directory_uri() , '/style.css');
-
-}
-
-add_action('wp_enqueue_scripts', 'luma_get_stylesheet');
-
-
-function enqueue_bootstrap(){
+<?php 
+function enqueue_styles(){
     wp_enqueue_style('bootstrap-cdn-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
+    wp_enqueue_style('style', get_stylesheet_directory_uri() , 'style.css');
+    wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_styles');
+
+function enqueue_scripts() {
     wp_enqueue_script('bootstrap-cdn-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js');
+
 }
-
-add_action('wp_enqueue_scripts','enqueue_bootstrap');
-
-
-function enqueue_fa(){
-    wp_enqueue_script('fa', 'https://kit.fontawesome.com/b2340e7a76.js');
-}
-
-add_action('wp_enqueue_scripts', 'enqueue_fa');
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
 ?>
