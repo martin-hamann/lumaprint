32f3ca5d1d8c1ee0b502fe00be8a37fa29d79ff1
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-26 13:33:03 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,13 +2,13 @@
 function enqueue_styles(){
     wp_enqueue_style('bootstrap-cdn-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css');
     wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
-    wp_enqueue_style( 'font-awesome-free', '//use.fontawesome.com/releases/v5.6.3/css/all.css');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_styles');
 
 function enqueue_scripts() {
     wp_enqueue_script('bootstrap-cdn-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js');
+    wp_enqueue_style('font-awesome', 'https://kit.fontawesome.com/b2340e7a76.js');
 
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts');
