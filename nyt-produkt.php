2a0d262c8fc62302930d5a8b65cd437802ec427c
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-27 09:56:53 +0100

    Første forsøg - nyt produkt dynamic

---
+++ nyt-produkt.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+ * Template Name: Nyt Produkt
+ */
+
+get_header();
+
+while (have_posts()):
+    the_post();
+    $overskrift = get_field('overskrift');
+    $beskrivelse = get_field('beskrivelse');
+    $billede = get_field('billede');
+    ?>
+
+    <div class="single-product container-fluid">
+        <h1>
+            <?php echo $overskrift ?>
+        </h1>
+        <p>
+            <?php echo $beskrivelse ?>
+        </p>
+        <img src="<?php echo $billede ?>" alt="<?php echo $overskrift ?>">
+
+    </div>
+
+<?php endwhile;
+get_footer();
+?>
