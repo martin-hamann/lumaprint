563b867c26b3e3d22df262c0bdfd5b0635320d98
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-27 15:58:44 +0100

    Update nyt-produkt.php

--- nyt-produkt.php
+++ nyt-produkt.php
@@ -25,53 +25,6 @@ while (have_posts()):
         </p>
         <img src="<?php echo $billede ?>" alt="<?php echo $overskrift ?>">
         <?php echo $table ?>
-        if ( ! empty ( $table ) ) {
-
-        echo '<table border="0">';
-
-            if ( ! empty( $table['caption'] ) ) {
-
-            echo '<caption>' . $table['caption'] . '</caption>';
-            }
-
-            if ( ! empty( $table['header'] ) ) {
-
-            echo '<thead>';
-
-                echo '<tr>';
-
-                    foreach ( $table['header'] as $th ) {
-
-                    echo '<th>';
-                        echo $th['c'];
-                        echo '</th>';
-                    }
-
-                    echo '</tr>';
-
-                echo '</thead>';
-            }
-
-            echo '<tbody>';
-
-                foreach ( $table['body'] as $tr ) {
-
-                echo '<tr>';
-
-                    foreach ( $tr as $td ) {
-
-                    echo '<td>';
-                        echo $td['c'];
-                        echo '</td>';
-                    }
-
-                    echo '</tr>';
-                }
-
-                echo '</tbody>';
-
-            echo '</table>';
-        }
     </div>
 
 <?php endwhile;
