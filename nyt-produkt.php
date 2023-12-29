f9529fb5395a7aaa3c7615a5a2bf9ee471835dce
martin-hamann <martinhamannnielsen@gmail.com>
2023-12-29 12:07:30 +0100

    Update nyt-produkt.php

--- nyt-produkt.php
+++ nyt-produkt.php
@@ -12,7 +12,6 @@ while (have_posts()):
     the_post();
     $overskrift = get_field('overskrift');
     $beskrivelse = get_field('beskrivelse');
-    $billede = get_field('billede');
     $table = get_field('table_shortcode');
     $gallery = get_field('gallery_shortcode');
     ?>
@@ -24,13 +23,12 @@ while (have_posts()):
         <p class="text">
             <?php echo $beskrivelse ?>
         </p>
-        <img src="<?php echo $billede ?>" alt="<?php echo $overskrift ?>">
-        <div class="row">
+        <div class="row single-product-row">
             <div class="col-10 col-lg-8">
                 <?php echo do_shortcode($table); ?>
             </div>
         </div>
-        <div class="row">
+        <div class="row single-product-row">
             <div class="col-10 col-lg-8">
                 <?php echo do_shortcode($gallery); ?>
             </div>
